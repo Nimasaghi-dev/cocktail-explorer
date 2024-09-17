4b5e3c23d0c506bd823661b2376bdf87aa3da8a8
Nima saghikalhori <nimasaghi.dev@gmail.com>
2024-09-17 14:12:17 +0200

    add-scroll-to-top

--- app.js
+++ app.js
@@ -103,3 +103,14 @@ function showLoadingSpinner(show) {
     }
 }
 
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollTopBtn.style.display = 'block';
+    } else {
+        scrollTopBtn.style.display = 'none';
+    }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
